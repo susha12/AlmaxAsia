8147883715b4d4eb8adc3f578be280bc81a7e243
susha12 <87581584+susha12@users.noreply.github.com>
2022-06-13 19:30:47 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -276,3 +276,22 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 
 });
+
+// плавный скрол
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+        const topOffset = 121; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
